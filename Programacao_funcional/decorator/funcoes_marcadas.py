bcd937b9e976e0d3dfbbf4e6f63830a6461209c0
Flavio <flavioantonio5@hotmail.com>
2022-05-08 16:16:09 -0300

    decorator marcador

---
+++ Programacao_funcional/decorator/funcoes_marcadas.py
@@ -0,0 +1,16 @@
+from Programacao_funcional.decorator.registrador import get_marcadas, marcar
+
+def primeira():
+    pass
+
+primeira = marcar(primeira)
+
+@marcar
+def segunda():
+    pass
+
+if __name__ == '__main__':
+    for f in get_marcadas():
+        print(f.__name__)
+        primeira()
+        segunda()
